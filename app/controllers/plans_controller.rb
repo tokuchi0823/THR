f59f85fa4ca9dd758f51432441b7a273b60888de
EC2 Default User <ec2-user@ip-172-31-39-66.ap-northeast-1.compute.internal>
2020-03-06 10:03:49 +0000

    test30

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -74,7 +74,7 @@ class PlansController < ApplicationController
   {
    type: 'text',
    text: '新しい企画が作成されました！チェックしてみましょう！
-   https://twenty-hearts-riders-app.herokuapp.com/plans'
+   https://twenty-hearts-riders-app.herokuapp.com/plans/' + 'おひょ'
   }
   end
   
