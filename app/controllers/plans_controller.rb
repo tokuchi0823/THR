8021fe4cec02698815c0e2a828f66a34d72525f4
EC2 Default User <ec2-user@ip-172-31-39-66.ap-northeast-1.compute.internal>
2020-03-08 05:56:35 +0000

    line

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -13,9 +13,8 @@ class PlansController < ApplicationController
   end
   
   def notification
-    client.push_message("U68ca1b0726b554003dfd64c286791252",message2)
+    client.broadcas(message2)
     #lineuserId = ["U68ca1b0726b554003dfd64c286791252","U9ddf1d3dc77db2dd1579c7a1d96204c9"]
-    client.broadcast(message3)
   end
   
    helper_method :notification
