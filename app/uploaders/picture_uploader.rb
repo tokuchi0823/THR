5264279f765ab6bfd7de389c0be52b371de54f27
tokuchi0823 <tokuchi.ryu.th@gmail.com>
2020-03-31 05:15:23 +0000

    s3

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,10 +4,16 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include CarrierWave::MiniMagick
   process resize_to_limit: [250, 250]
+  
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
