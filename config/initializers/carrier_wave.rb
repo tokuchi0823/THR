491e2a4746e218e8d60dea6ced8747e082ebf5be
tokuchi0823 <tokuchi.ryu.th@gmail.com>
2020-04-06 05:26:54 +0000

    test22

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,8 +5,10 @@ if Rails.env.production?
       # Amazon S3用の設定
       :provider              => 'AWS',
       :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+   #   :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+    #  :aws_secret_access_key => ENV['S3_SECRET_KEY']
+       :aws_access_key_id => Rails.application.credentials.aws[:access_key_id],
+       :aws_secret_access_key => Rails.application.credentials.aws[:secret_access_key]
     }
     config.fog_directory     =  ENV['S3_BUCKET']
     config.asset_host = 'https://thr-app.s3.amazonaws.com' #このように修正
